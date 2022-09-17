511787493a38859d3d09d25340b546726c2957e2
reemmahmoud <reemmahmoud726@gmail.com>
2022-09-17 02:49:53 +0200

    complete xcite website

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+(function($) {
+	console.log("in main file")
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
